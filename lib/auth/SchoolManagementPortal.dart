e9c7aebee012bb7b0aad9b0b64837613d52d3202
sahil jaiswal <sahil1905050310026@gmail.com>
2023-11-10 08:10:44 +0530

    updated 11-10-2023

--- lib/auth/SchoolManagementPortal.dart
+++ lib/auth/SchoolManagementPortal.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:ispark/auth/SchoolManagementPortal.dart';
-import 'package:ispark/screens/Dashboard.dart';
 
-class AuthScreen extends StatefulWidget {
-  const AuthScreen({super.key});
+class SchoolManagementPortal extends StatefulWidget {
+  const SchoolManagementPortal({super.key});
 
   @override
-  State<AuthScreen> createState() => _AuthScreenState();
+  State<SchoolManagementPortal> createState() => _SchoolManagementPortalState();
 }
 
-class _AuthScreenState extends State<AuthScreen> {
+class _SchoolManagementPortalState extends State<SchoolManagementPortal> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    TextEditingController usertype = new TextEditingController();
+    usertype.text = "ISpark Management";
 
     return Scaffold(
       body: Column(
@@ -21,7 +21,7 @@ class _AuthScreenState extends State<AuthScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              "Login",
+              "Management Sign Up",
               style: TextStyle(
                   fontSize: 28,
                   color: const Color.fromARGB(255, 2, 73, 132),
@@ -33,7 +33,7 @@ class _AuthScreenState extends State<AuthScreen> {
             Align(
               alignment: Alignment.center,
               child: SizedBox(
-                height: height * 0.5,
+                height: height * 0.7,
                 width: width * 0.4,
                 child: Card(
                   child: SizedBox(
@@ -88,6 +88,55 @@ class _AuthScreenState extends State<AuthScreen> {
                         ),
                       ),
                       SizedBox(
+                        height: 5,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: FractionallySizedBox(
+                          widthFactor: 0.8,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5.0),
+                                border: Border.all(
+                                    width: 1,
+                                    color: Color.fromARGB(255, 122, 122, 122))),
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Repeat Password",
+                                    border: InputBorder.none),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: FractionallySizedBox(
+                          widthFactor: 0.8,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5.0),
+                                border: Border.all(
+                                    width: 1,
+                                    color: Color.fromARGB(255, 122, 122, 122))),
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              child: TextField(
+                                controller: usertype,
+                                decoration: InputDecoration(
+                                    hintText: "User Type",
+                                    border: InputBorder.none),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
                         height: 20,
                       ),
                       Align(
@@ -109,10 +158,7 @@ class _AuthScreenState extends State<AuthScreen> {
                                         fontWeight: FontWeight.bold),
                                   ),
                                 ),
-                                onPressed: () {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (context) => DashBoard()));
-                                },
+                                onPressed: () {},
                               ),
                             ),
                           ),
@@ -128,16 +174,10 @@ class _AuthScreenState extends State<AuthScreen> {
                       SizedBox(
                         height: 5,
                       ),
-                      InkWell(
-                        onTap: () {
-                          // Navigator.of(context).push(MaterialPageRoute(
-                          //     builder: (context) => SchoolManagementPortal()));
-                        },
-                        child: Text(
-                          "Sign Up as Management",
-                          style: TextStyle(
-                              color: const Color.fromARGB(255, 2, 73, 132)),
-                        ),
+                      Text(
+                        "Sign Up as Management",
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 2, 73, 132)),
                       ),
                       SizedBox(
                         height: 5,
--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -8,8 +8,153 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  late bool cardhover;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cardhover = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("COEMS"))),
+      drawer: Drawer(
+        child: Column(children: []),
+      ),
+      backgroundColor: Color.fromARGB(255, 235, 235, 235),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: height * 0.4,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: Card(
+                        child: Column(
+                          children: [
+                            Expanded(
+                                flex: 8,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(15.0),
+                                  child:
+                                      Image(image: AssetImage("ringing.png")),
+                                )),
+                            Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Daily Updates",
+                                  style: TextStyle(fontSize: 16),
+                                )),
+                          ],
+                        ),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                  flex: 8,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Image(
+                                        image: AssetImage("calendar.png")),
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    "Monthly Session Details",
+                                    style: TextStyle(fontSize: 16),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                  flex: 8,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child:
+                                        Image(image: AssetImage("chart.png")),
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    "Analysis",
+                                    style: TextStyle(fontSize: 16),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                  flex: 8,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Image(image: AssetImage("city.png")),
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    "School Details",
+                                    style: TextStyle(fontSize: 16),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                  flex: 8,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Image(
+                                        image: AssetImage("calculator.png")),
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    "Lab Equipment",
+                                    style: TextStyle(fontSize: 16),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
